04878bfb970fdfdd83679b4f924187bef9f26f84
Bevis <bevis_chen2@soft-world.com.tw>
2025-11-24 09:40:23 +0800

    feat(debug): Add randomImageURL utility function

--- Sources/Utilities/Debug.swift
+++ Sources/Utilities/Debug.swift
@@ -1,3 +1,9 @@
+import Foundation
+
 public func memoryAddress(_ object: AnyObject) -> UnsafeMutableRawPointer {
     Unmanaged.passUnretained(object).toOpaque()
+}
+
+public func randomImageURL(width: Int = 50, height: Int = 50) -> URL {
+    return URL(string: "https://picsum.photos/\(width)/\(height)")!
 }
